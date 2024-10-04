92a54a3b20e1c366672ebc527d95c31b726a272c
Mikhail Yesipchuk <domovou69@gmail.com>
2024-10-04 19:23:09 +0300

    feat: implement args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv;
+
+  args.forEach((arg, index) => {
+    if (arg.startsWith("--")) {
+      const value = args[index + 1];
+      if (value && !value.startsWith("--")) {
+        console.log(`${arg.slice(2)} is ${value}`);
+      }
+    }
+  });
 };
 
 parseArgs();
