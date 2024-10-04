547688a09279206a9bd8562bba794dffd1d06465
Mikhail Yesipchuk <domovou69@gmail.com>
2024-10-04 19:27:39 +0300

    feat: implement worker.js

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,22 @@
+import { Worker, isMainThread, parentPort } from "node:worker_threads";
+import { fileURLToPath } from "node:url";
 // n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  if (isMainThread) {
+    const worker = new Worker(fileURLToPath(import.meta.url));
+    worker.on("message", (message) => {
+      console.log(`Fibonacci result: ${message}`);
+    });
+    worker.postMessage("15");
+  } else {
+    parentPort.on("message", (mainThreadMsg) => {
+      const result = nthFibonacci(mainThreadMsg);
+      parentPort.postMessage(result);
+    });
+  }
 };
 
 sendResult();
