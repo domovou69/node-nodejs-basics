5bacde5a68c6ed146a6c85b651c2f634d9e89cac
Mikhail Yesipchuk <domovou69@gmail.com>
2024-10-04 19:24:47 +0300

    feat: implement list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,23 @@
+import { mkdir, opendir, copyFile } from "fs/promises";
+import { existsSync } from "fs";
+import { fileURLToPath } from "url";
+import path from "path";
+
 const list = async () => {
-    // Write your code here 
+  const curFilePath = fileURLToPath(import.meta.url);
+  const curFolder = path.dirname(curFilePath);
+  const fromFilePath = path.join(curFolder, "files");
+
+  if (!existsSync(fromFilePath)) {
+    throw new Error("FS operation failed");
+  }
+
+  const dir = await opendir(fromFilePath);
+  let allFileNamesArr = [];
+  for await (const dirent of dir) {
+    allFileNamesArr.push(dirent.name);
+  }
+  console.log(allFileNamesArr);
 };
 
 await list();
