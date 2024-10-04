1ad8eb8c99b66fc17ed9c2445996d35474069257
Mikhail Yesipchuk <domovou69@gmail.com>
2024-10-04 19:27:11 +0300

    feat: implement write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import { createWriteStream } from "fs";
+import { fileURLToPath } from "url";
+import path from "path";
+
 const write = async () => {
-    // Write your code here 
+  const pathToDestinationFile = path.resolve(
+    path.dirname(fileURLToPath(import.meta.url)),
+    "files",
+    "fileToWrite.txt"
+  );
+
+  const streamWrite = createWriteStream(pathToDestinationFile);
+
+  process.stdin.pipe(streamWrite);
 };
 
 await write();
