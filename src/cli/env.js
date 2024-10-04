c1e5f14eaf7fc651c6a7745fc91c9a46ced39999
Mikhail Yesipchuk <domovou69@gmail.com>
2024-10-04 19:23:30 +0300

    feat: implement env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,16 @@
 const parseEnv = () => {
-    // Write your code here 
+  process.env.RSS_stable = "QA";
+  process.env.RSS_develop = "DEV";
+
+  const envVars = process.env;
+  const rssVars = Object.entries(envVars).filter(([key]) =>
+    key.startsWith("RSS_")
+  );
+  const formattedVars = rssVars
+    .map(([key, value]) => `${key}=${value}`)
+    .join("; ");
+
+  console.log(formattedVars);
 };
 
 parseEnv();
